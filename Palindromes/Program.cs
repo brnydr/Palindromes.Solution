c1c83f221db09a1200a0e36e45001497b2c3c0e9
brnydr <brianwilliamyoder@gmail.com>
2023-08-30 19:08:25 -0700

    create background and foreground color

--- Palindromes/Program.cs
+++ Palindromes/Program.cs
@@ -1,5 +1,6 @@
 using Palindromes.Models;
 using System;
+using System.Threading;
 
 namespace Palindromes
 {
@@ -7,10 +8,15 @@ namespace Palindromes
     {
       static void Main()
       {
+
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
         Console.WriteLine("Enter a word to check if it is a palindrome:");
         string input = Console.ReadLine();
         Checker newChecker = new Checker(input);
         Console.WriteLine(newChecker.IsPalindrome());
+        
+      
       }
     }
 }
